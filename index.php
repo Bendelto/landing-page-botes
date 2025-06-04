69320379289b8a8bef754d017b229fc5c988dcf5
Bendelto <benko27@hotmail.com>
2025-06-04 15:10:01 -0500

    Title modificado

--- index.php
+++ index.php
@@ -106,7 +106,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Alquila un Bote en Cartagena - Islas del Rosario y Atardeceres</title>
+    <title>Alquiler de Botes y yates en Cartagena</title>
 	<link rel="icon" href="logo.svg" type="image/svg+xml">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/css/intlTelInput.min.css">
